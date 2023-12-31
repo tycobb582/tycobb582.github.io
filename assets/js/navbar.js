009a0bf12068924e05b89e07f588de28c16e8376
GoldenKnight582 <tycobb582@gmail.com>
2023-12-30 20:31:15 -0500

    About Page, Projects Dropdown, and Style
    -Moved picture of self and introduction to an About page
    -Added "Projects" drop down item on the navbar, which links to every project page
    -Added hover highlights to navbar elements
    -Changed BP code images to use embedded BP pastebin elements
    -Modified style of project page content to be much more readable

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,14 +1,21 @@
 var lastScrollTop = 0;
 navbar = document.getElementById("navbar");
+drop = document.getElementById("drop");
+droplist = document.getElementById("droplist");
+
+drop.addEventListener("mouseover", (event) => {
+    droplist.style.pointerEvents = "auto";
+})
+
 window.addEventListener("scroll", function(){
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    var scrollTop = window.scrollY || document.documentElement.scrollTop;
     if (scrollTop > lastScrollTop)
     {
         navbar.style.top = "-80px";
+        droplist.style.pointerEvents = "none";
     }
     else
     {
-        console.log(navbar.style.top);
         navbar.style.top = "0";
     }
     lastScrollTop = scrollTop;
